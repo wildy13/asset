db77c5b3ff331388b869c3f084053ba5d6080beb
wildy13 <wildy.simanjuntak13@gmail.com>
2022-11-22 10:22:49 +0700

    change posisition of search

--- frontend/components/index/asset.vue
+++ frontend/components/index/asset.vue
@@ -26,7 +26,7 @@
         <div>
           <!-- Search -->
           <input
-            class=" placeholder:text-slate-400 bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-gray-800 focus:ring-1 sm:text-sm "
+            class=" placeholder:text-slate-400 bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-gray-800 focus:ring-1 sm:text-sm mr-4"
             placeholder="search data in Here"
           />
         </div>
