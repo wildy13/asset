393039fa05237ff0e710061941e5956ca328e16f
wildy13 <wildy.simanjuntak13@gmail.com>
2023-03-10 13:38:32 +0700

    header modified

--- frontend/components/general/Header.vue
+++ frontend/components/general/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-full">
-    <div class="flex bg-slate-600">
+    <div class="flex bg-gradient-to-r from-slate-600 to-slate-900 p-2">
       <div><img src="/img/logo.png" alt="labtechLogo.png" class="w-32" /></div>
-      <div class="font-serif italic flex items-center w-full justify-center text-4xl xl:bg-red">
+      <div class="font-serif italic text-white flex items-center w-full justify-center text-4xl xl:bg-red">
         Asset Data System
       </div>
     </div>
